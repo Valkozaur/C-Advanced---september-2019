51b586799f6276d95a99260418854258aa8cc739
Valkozaur <val.Nikolov03@gmail.com>
2020-02-09 17:11:22 +0100

    New Commit

---
+++ StacksAndQueues-Exercise/1.Sum Matrix Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixDimensions[0];
+            int columns = matrixDimensions[1];
+
+            int[,] matrix = new int[rows, columns];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine
+                (
+                matrix.GetLength(0) + Environment.NewLine +
+                matrix.GetLength(1) + Environment.NewLine +
+                sum
+                );
+        }
+    }
+}
---
+++ StacksAndQueues-Exercise/3. Primary Diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] squareMatrix = new int[size, size];
+
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                int[] numbersInRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < squareMatrix.GetLength(1); col++)
+                {
+                    squareMatrix[row, col] = numbersInRow[col];
+                }
+            }
+
+            int sum = 0;
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                sum += squareMatrix[row, row];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
