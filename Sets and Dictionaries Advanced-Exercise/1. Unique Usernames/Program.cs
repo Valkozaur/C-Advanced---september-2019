886dee16882f95a272a92267b869808569feb8f5
Valkozaur <val.Nikolov03@gmail.com>
2020-02-14 00:03:28 +0100

    Sets and Dictionaries new Exercises

---
+++ Sets and Dictionaries Advanced-Exercise/1. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, uniqueNames));
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced-Exercise/3. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var chemicalElementsInput = Console.ReadLine()
+                    .Split(" ");
+
+                foreach (var chemicalElement in chemicalElementsInput)
+                {
+                    periodicTable.Add(chemicalElement);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", periodicTable));
+        }
+    }
+}
