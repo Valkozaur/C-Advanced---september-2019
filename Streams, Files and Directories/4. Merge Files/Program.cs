9eda6835783c92f22b2f18e4add0f96418b6bb29
Valkozaur <val.Nikolov03@gmail.com>
2020-02-18 00:17:10 +0100

    Streams

--- Streams, Files and Directories/4. Merge Files/Program.cs
+++ Streams, Files and Directories/4. Merge Files/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _4._Merge_Files
 {
@@ -6,7 +7,20 @@ namespace _4._Merge_Files
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var firstFile = new StreamReader("Text1.txt"))
+            using (var secondFile = new StreamReader("Text2.txt"))
+            using (var mergedFiles = new StreamWriter("MergedFiles.txt"))
+            {
+                while (!firstFile.EndOfStream && !secondFile.EndOfStream)
+                {
+                    var firstLine = firstFile.ReadLine();
+                    var secondLine = secondFile.ReadLine();
+
+                    mergedFiles.WriteLine(firstLine);
+                    mergedFiles.WriteLine(secondLine);
+
+                }
+            }
         }
     }
 }
---
+++ Streams, Files and Directories/5. Slice a File/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace _5._Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFile = @"C:\Users\valni\OneDrive\Desktop\SoftUni\Repositorys\Advanced-C#-2019\Streams, Files and Directories\5. Slice a File\sliceMe.txt";
+            var parts = 4;
+            var fileLenght = new FileInfo("sliceMe.txt").Length;
+            var fileSize = (int)(fileLenght / parts);
+            var fileNumber = 1;
+
+            using (var fileToSlice = new StreamReader(sourceFile))
+            {
+                while (!fileToSlice.EndOfStream)
+                {
+                    var bufferSize = fileSize;
+                    if (fileNumber == parts)
+                    {
+                        bufferSize = (int)fileLenght - (parts - 1) * fileSize;
+                    }
+
+                    var buffer = new char[bufferSize];
+                    var files = fileToSlice.Read(buffer, 0, buffer.Length);
+
+                    using (var filesToCreate = new StreamWriter($"Part-{fileNumber++}.txt"))
+                    {
+                        filesToCreate.Write(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
