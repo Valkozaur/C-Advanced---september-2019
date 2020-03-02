5bf977fd078b31d749ea7f9f1f5028ac1c97d458
Valkozaur <val.Nikolov03@gmail.com>
2020-03-02 13:51:06 +0100

    LinkedList - More Work Needed!

--- LinkedList-selfwritten/LinkedList-selfwritten/Program.cs
+++ LinkedList-selfwritten/LinkedList-selfwritten/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LinkedList_selfwritten
 {
@@ -6,7 +7,6 @@ namespace LinkedList_selfwritten
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
